b280aea7f9e8b32e04941f8d0f3329e11d36114d
ayush contractor <ayushcon2000@gmail.com>
2023-03-03 15:02:52 +0530

    new changes vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,8 @@
-echo " BUILD START"
+echo " Build start..."
 python3.9 -m pip install -r requirements.txt
+echo "Making Migrations..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+echo "Collecting static..."
 python3.9 manage.py collectstatic --noinput --clear
 echo " BUILD END"
