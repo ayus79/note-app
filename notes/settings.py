cc87c07eef6dc6229c825418dcee596b752682a3
ayush contractor <ayushcon2000@gmail.com>
2023-03-03 15:16:39 +0530

    railway commit

--- notes/settings.py
+++ notes/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 import os
 from decouple import config
 import whitenoise
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -67,14 +67,7 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES={
-   'default':{
-      'ENGINE':'django.db.backends.postgresql_psycopg2',
-      'NAME':'note',
-      'USER':'postgres',
-      'PASSWORD':'admin',
-      'HOST':'localhost',
-      'PORT':'5432',
-   }
+   'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800) ,
 }
 
 
